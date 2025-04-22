3c7679058720fa5ac3c07cb59e25883b0fc99294
Aakriti Bhandari <aakritibhandari62@gmail.com>
2025-04-22 12:21:01 +0545

    Completed

--- base/models.py
+++ base/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 #USER MODEL
-
 class User(AbstractUser):
-
     ROLE_CHOICES=[
         ('librarian', 'Librarian'),
         ('member', 'Member')
-    ]
+    ]       #CHOICES OF ROLE TO PROVIDE PERMISSIONS 
+
     username=models.CharField(max_length=300, unique=True)
     password=models.CharField(max_length=300)
     email=models.EmailField()
@@ -24,24 +23,23 @@ class Book(models.Model):
        ('novel', 'Novel'),
        ('poem', 'Poem'),
        ('story', 'Story')
-    ]
+    ]       #CHOICES FOR USER TO NAVIGATE THROUGH THE LIBRARY DATA EASILY
 
     title=models.CharField(max_length=300, unique=True)
     author=models.CharField(max_length=300)
     published_date=models.DateField()
     genre=models.CharField(max_length=100, choices=GENRE_CHOICES)
 
-    def __str__(self):
+    def __str__(self):      #TO DISPLAY THE NAME OF THE TITLE INSTEAD OF THE OBJECT IN THE ADMIN PANNEL
         return self.title
 
 
 #RECORD MODEL
-
 class BorrowingRecord(models.Model):
     STATUS_CHOICES=[
         ('returned', 'Returned'),
         ('borrowed', 'Borrowed')
-    ]
+    ]       
     borrower=models.ForeignKey(User, on_delete=models.CASCADE)
     borrowed_book=models.ForeignKey(Book, on_delete=models.CASCADE)
     borrowed_at=models.DateField(auto_now_add=True)
@@ -49,5 +47,5 @@ class BorrowingRecord(models.Model):
     return_date=models.DateField(null=True, blank=True)
     status=models.CharField(max_length=50, choices=STATUS_CHOICES, default='borrowed')
     
-    def __str__(self):
+    def __str__(self):      #TO DISPLAY THE STRING PROVIDED INSTEAD OF THE OBJECT
         return f"{self.borrower} borrowed {self.borrowed_book}"
--- base/serializers.py
+++ base/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers   
 from datetime import timedelta
 from django.utils import timezone
 from .models import *
@@ -7,13 +7,13 @@ from .models import *
 #USER SERIALIZER
 
 class UserSerializer(serializers.ModelSerializer):
-    password=serializers.CharField(write_only=True, min_length=6)
+    password=serializers.CharField(write_only=True, min_length=6)   #TO MAKE PASSWORD FIELD MORE FLEXIBLE AND REAL
     class Meta:
         model=User
         fields=['username', 'password', 'email', 'role', 'address', 'contact']
 
     
-    def create(self, validated_data):
+    def create(self, validated_data):       #TO CREATE THE USER TABLE 
         user=User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
@@ -38,14 +38,15 @@ class BorrowingRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model=BorrowingRecord
         fields=['status','borrower', 'borrower_name', 'borrowed_book', 'borrowed_at', 'due_date', 'return_date']
-        read_only_fields=['borrower','due_date']
+        read_only_fields=['borrower','due_date']        #USER CANNOT POST DATAS IN THESE FIELDS WITH THE HELP OF READ ONLY FIELDS
 
-    def get_borrower_name(self, obj):
+
+    def get_borrower_name(self, obj):       #HELPS TO DISPLAY THE NAME OF THE USER INSTEAD OF THEIR ID
         return obj.borrower.username
 
     def create(self, validated_data):
-        borrowed_at=timezone.now().date()
-        due_date=borrowed_at+timedelta(days=30)
+        borrowed_at=timezone.now().date()       #IT IS OPERATED HERE BECAUSE WE NEED HELP OF THIS VAIRABLE TO CREATE DUE DATE
+        due_date=borrowed_at+timedelta(days=30)     #TIME DELTA HELPS IN ADDING EXTRA 30 DAYS IN THE CURRENT DAY STORED IN BORROWED_AT VARIABLE
         validated_data['due_date']=due_date
         user=self.context['request'].user
         validated_data['borrower']=user
